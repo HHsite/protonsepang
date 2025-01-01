61e9a035c1a7db49b64ad9caa48e9e0144821534
HHsite <haziq_hamidi@hotmail.com>
2025-01-01 21:49:33 +0800

    update

--- src/card-swiper.jsx
+++ src/card-swiper.jsx
@@ -14,14 +14,22 @@ import sagaCar from './assets/saga-car.png';
 import irizCar from './assets/iriz-car.png';
 import exoraCar from './assets/exora-car.png';
 
+import x50Pricelist from './assets/x50-pricelist.pdf';
+import x70Pricelist from './assets/x70-pricelist.pdf';
+import x90Pricelist from './assets/x90-pricelist.pdf';
+import s70Pricelist from './assets/s70-pricelist.pdf';
+import personaPricelist from './assets/persona-pricelist.pdf';
+import sagaPricelist from './assets/saga-pricelist.pdf';
+import irizPricelist from './assets/iriz-pricelist.pdf';
+
 const carData = [
-  { name: "X50", type: "Compact Crossover SUV", imageUrl: x50Car, url: x50Car},
-  { name: "X70", type: "Mid-size SUV", imageUrl: x70Car, url: x70Car },
-  { name: "X90", type: "Full-size SUV", imageUrl: x90Car, url: x90Car },
-  { name: "S70", type: "Mid-size Sedan", imageUrl: s70Car, url: s70Car },
-  { name: "PERSONA", type: "Compact Sedan", imageUrl: personaCar, url: personaCar },
-  { name: "SAGA", type: "Compact Sedan", imageUrl: sagaCar, url: sagaCar },
-  { name: "IRIZ", type: "Hatchback", imageUrl: irizCar, url: irizCar },
+  { name: "X50", type: "Compact Crossover SUV", imageUrl: x50Car, url: x50Pricelist},
+  { name: "X70", type: "Mid-size SUV", imageUrl: x70Car, url: x70Pricelist },
+  { name: "X90", type: "Full-size SUV", imageUrl: x90Car, url: x90Pricelist },
+  { name: "S70", type: "Mid-size Sedan", imageUrl: s70Car, url: s70Pricelist },
+  { name: "PERSONA", type: "Compact Sedan", imageUrl: personaCar, url: personaPricelist },
+  { name: "SAGA", type: "Compact Sedan", imageUrl: sagaCar, url: sagaPricelist },
+  { name: "IRIZ", type: "Hatchback", imageUrl: irizCar, url: irizPricelist },
   { name: "EXORA", type: "MPV", imageUrl: exoraCar, url: exoraCar },
 ];
 
@@ -33,7 +41,7 @@ export default function SwiperApp() {
 
   return (
     <>
-      <p className="text-center mt-10 font-bold text-xl">Explore our Proton</p>
+      <p className="text-center mt-10 font-bold text-2xl">Explore our Proton</p>
       <Swiper
         slidesPerView={"auto"}
         spaceBetween={30}
--- src/service-component.jsx
+++ src/service-component.jsx
@@ -1,38 +1,33 @@
-import {Card, CardFooter, Image, Button, CardHeader} from "@nextui-org/react";
-import x50Car from './assets/x50-car.png';
+import {Card, CardBody, Image, Button, CardHeader, Divider} from "@nextui-org/react";
+import x50Car from './assets/trade-in.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp, faCreativeCommonsPdAlt } from '@fortawesome/free-brands-svg-icons';
 
 const servData = [
-  { desc: "Bosan dengan kereta lama? Kami offer trade-in dengan harga yang sangat tinggi! Dapatkan harga yang lebih tinggi dari pasaran, dan upgrade ke kereta baru tanpa stress. No hidden charges, just a fair deal!", type: "Trade-in Kereta (Harga Tinggi)", imageUrl: x50Car},
-  { desc: "Nak plat nombor kereta yang unik atau yang catchy? Kami sediakan servis beli atau tukar plat nombor kereta — dari yang standard sampai yang lebih eksklusif. Pilih je yang paling sesuai dengan personaliti anda!", type: "No Plat Kereta (Beli atau Swap)", imageUrl: x50Car },
-  { desc: "Perlukan loan untuk kereta? Jangan risau, kami ada banyak pilihan bank loan dengan interest rate yang competitive. Kami tolong settlekan semua paperwork dan urusan bank untuk anda, so you can get your dream car faster!", type: "Bank Loan (Pelbagai Pilihan)", imageUrl: x50Car },
-  { desc: "Jangan pening kepala dengan urusan JPJ dan Puspakom. Kami uruskan semua benda tu untuk anda, dari pendaftaran kereta sampai inspection. Semua proses jadi cepat dan mudah, tanpa perlu buang masa beratur!", type: "Urusan JPJ dan Puspakom", imageUrl: x50Car },
-  { desc: "Taknak keluar rumah untuk renew road tax atau insurance? Kami boleh uruskan semua untuk anda, cepat dan mudah. Tak payah queue, harga pun confirm reasonable — so, you can drive worry-free!", type: "Renew Road Tax dan Insurance", imageUrl: x50Car },
+  { desc: "Bosan dengan kereta lama? Kami offer trade-in dengan harga yang sangat tinggi! Dapatkan harga yang lebih tinggi dari pasaran, dan upgrade ke kereta baru tanpa stress. No hidden charges, just a fair deal!", type: "Trade-in Kereta (Harga Tinggi)", imageUrl: "swap_driving_apps"},
+  { desc: "Nak plat nombor kereta yang unik atau yang catchy? Kami sediakan servis beli atau tukar plat nombor kereta — dari yang standard sampai yang lebih eksklusif. Pilih je yang paling sesuai dengan personaliti anda!", type: "No Plat Kereta (Beli atau Swap)", imageUrl: "pin" },
+  { desc: "Perlukan loan untuk kereta? Jangan risau, kami ada banyak pilihan bank loan dengan interest rate yang competitive. Kami tolong settlekan semua paperwork dan urusan bank untuk anda, so you can get your dream car faster!", type: "Bank Loan (Pelbagai Pilihan)", imageUrl: "assured_workload" },
+  { desc: "Jangan pening kepala dengan urusan JPJ dan Puspakom. Kami uruskan semua benda tu untuk anda, dari pendaftaran kereta sampai inspection. Semua proses jadi cepat dan mudah, tanpa perlu buang masa beratur!", type: "Urusan JPJ dan Puspakom", imageUrl: "local_police" },
+  { desc: "Taknak keluar rumah untuk renew road tax atau insurance? Kami boleh uruskan semua untuk anda, cepat dan mudah. Tak payah queue, harga pun confirm reasonable — so, you can drive worry-free!", type: "Renew Road Tax dan Insurance", imageUrl: "source_notes" },
 ];
 
 export default function ServiceComponent() {
   return (
     <>
     <div className="bg-stone-50 py-10">
-    <p className="text-center mt-10 font-bold text-xl">Servis Yang Saya Sediakan</p>
+    <p className="text-center mt-10 font-bold text-2xl">Servis Yang Saya Sediakan</p>
     <div className="w-[90%] sm:w-[80%] my-10 mx-auto flex flex-wrap gap-10 items-center justify-center">
         {servData.map((serv, index) => (
-        <Card isFooterBlurred isPressable className="border-none w-80" radius="lg" key={index}>
-            <CardHeader className="flex bg-black/30 items-start overflow-hidden h-full absolute z-20 opacity-0 hover:opacity-100 transition-opacity duration-300">
-                <p className="text-white text-sm text-start">{serv.desc}</p>
-            </CardHeader>
-            <Image
-                alt="Woman listing to music"
-                className="object-cover w-auto relative"
-                src="https://nextui.org/images/hero-card.jpeg"
-            />
-            <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                <p
-                className="font-semibold text-white bg-black/0 py-2"
-                >
-                {serv.type}
-                </p>
-            </CardFooter>
-        </Card>
+            <Card  isPressable className="w-[400px] h-[250px]" radius="lg" key={index}>
+                 <CardHeader className="flex gap-3 bg-gradient-to-r from-cyan-500 to-blue-500">
+                 <span className="material-symbols-outlined bg-gradient-to-r from-cyan-200 to-blue-200 bg-clip-text text-transparent text-4xl">{serv.imageUrl}</span>
+                    <p className="text-md font-bold text-white">{serv.type}</p>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <p>{serv.desc}</p>
+                </CardBody>
+            </Card>
          ))}
     </div>
     </div>
